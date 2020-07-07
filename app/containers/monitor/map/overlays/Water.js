a981d9b55cb95c9f715aefa7b6ec626d9a9b64c2
phz <hnphz76@gmail.com>
2020-07-07 11:35:28 +0800

    修改错误

--- app/containers/monitor/map/overlays/Water.js
+++ app/containers/monitor/map/overlays/Water.js
@@ -16,22 +16,27 @@ class Water extends Base {
   static type = "water";
   constructor(props, context) {
     super(props, context);
+    const {model} = props;
+    const {videos} = model;
+    let token = videos && videos.length !== 0 ? videos[0].strtoken : '';
     this.state = {
-      token: ""
+      token: token
     };
     this.onClose = this.onClose.bind(this);
   }
   render() {
     let { model } = this.props;
-    let water = (model.z !== null && model.z !== undefined) ? (model.z + 'm') : '--';
-    let udpTm = model.tm ? moment(model.tm).format('MM-DD HH:mm') : '--';
+    //let water = (model.z !== null && model.z !== undefined) ? (model.z + 'm') : '--';
+    let water = model.z ? parseFloat(model.z) : NaN;
+    let waterString = isNaN(water) ? '--' : (water.toFixed(2) + 'm');
+    let tm = model.ztm ? model.ztm : model.tm;
+    let udpTm = tm ? moment(tm).format('MM-DD HH:mm') : '--';
     let warningLevel = (model.warning && model.warning !== 99) ? (model.warning + 'm') : '--';
     let rivername = (model.rivername !== null) ? model.rivername : '--';
     let regionName = (model.regionName !== null) ? model.regionName : '--';
     let videoControl = model.videoControl;
     let videos = model.videos;
-    let token = videos && videos.length != 0 ? videos[0].strtoken : '';
-    this.setState({ token: token })
+    //let token = videos && videos.length != 0 ? videos[0].strtoken : '';
     let type = videos && videos.length != 0 ? videos[0].datasource : '';
     const onChange = (e) => {
       console.log(`radio checked:${e.target.value}`);
@@ -44,27 +49,16 @@ class Water extends Base {
           waterWar: false
         })
       }
-    }
+    };
     return (
       <div className="m-ovl-box m-ovl-water" style={{ display: "none", width: 900, height: 680 }} ref={(node) => { this.container = node; }}>
-        {/* <div>水位站点：{model.stnm || model.name}</div> */}
-        {/* <div className="m-ovl-line">水位站点：{model.stnm || model.name}</div> */}
-        {/* <div className="m-ovl-line">站点编号：{model.stcd}</div> */}
-        {/* <div className="m-ovl-line">数据来源：{model.dataSourceDesc}</div> */}
-        {/* <div className="m-ovl-line">站点地址：{model.address}</div> */}
-        {/* <div className="m-ovl-line">当前水位：{water}</div> */}
-        {/* <div className="m-ovl-line">警戒水位：{warningLevel}</div> */}
-        {/* <div className="m-ovl-line">更新时间：{udpTm}</div> */}
-        {/* <div style={{width: 660, height: 420}} id='echartsDiv'> */}
-        {/* </div> */}
-
         <div style={{
           float: 'left', width: 650, height: 400, position: 'relative',
           top: 275
         }}>
           <VideoComponent
             videoControl={videoControl}
-            token={token}
+            token={this.state.token}
             type={type}
             style={{
               width: 600,
@@ -80,7 +74,7 @@ class Water extends Base {
         <div style={{ float: 'right', overflow: 'hidden', width: 200 }}>
           <div>
             <div className="m-ovl-line" style={{ width: 180 }}>名称：{model.stnm || model.name}</div>
-            <div className="m-ovl-line">水位：{water}</div>
+            <div className="m-ovl-line">水位：{waterString}</div>
             <div className="m-ovl-line">来源：{model.dataSourceDesc}</div>
             <div className="m-ovl-line">河流：{rivername}</div>
             <div className="m-ovl-line">县区：{regionName}</div>
@@ -90,7 +84,7 @@ class Water extends Base {
           </div>
         </div>
         <span className="iconfont iconcuo m-ovl-close" ></span>
-        <Holder token={token} divStyle={{ top: 330, right: -30, transform: 'scale(0.7)', }}></Holder>
+        <Holder token={this.state.token} divStyle={{ top: 330, right: -30, transform: 'scale(0.7)', }}></Holder>
         <div style={{
           float: 'left', width: 660, height: 315, position: 'relative',
           left: -15,
@@ -140,7 +134,6 @@ class Water extends Base {
     // category = category.reverse();
     // values = values.reverse();
 
-    console.log(model)
     let chars = echarts.init(document.getElementById('echartsDiv'));
     let xdata = [];
     let ydata = [];
@@ -175,7 +168,7 @@ class Water extends Base {
       };
       chars.setOption(option);
     } else {
-      for (var i = model.waters.length - 1; i >= 0; i--) {
+      for (let i = model.waters.length - 1; i >= 0; i--) {
         xdata.push(moment(model.waters[i].tm).format('MM-DD HH:mm'))
         ydata.push(model.waters[i].z)
       }
