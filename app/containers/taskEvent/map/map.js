b8f36f73056208c761fd4fc66a03479531b4b0ba
phz <phz@tuhuitech.cn>
2020-11-03 16:51:01 +0800

    修改任务调度中的地图样式

--- app/containers/taskEvent/map/map.js
+++ app/containers/taskEvent/map/map.js
@@ -172,15 +172,6 @@ class Map extends React.PureComponent {
       zIndex: 11,
       key: "river40",
     });
-    // this.map.addGeo({
-    //   url: "http://code.tuhuitech.cn:10012/geoserver/dy/wms",
-    //   params: {
-    //     LAYERS: "dy:河流40",
-    //     TILED: true,
-    //   },
-    //   zIndex: 10,
-    //   key: "river40",
-    // });
 
     // 加入交通实况图;
     this.map.addGeo({
