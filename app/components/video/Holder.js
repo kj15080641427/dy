9c2a96ba6b06f2fcd3d04fce6c8f53d58af7c185
phz <phz@tuhuitech.cn>
2020-11-04 10:25:14 +0800

    重新实现视频组件，解决视频不能全屏的问题

--- app/components/video/Holder.js
+++ app/components/video/Holder.js
@@ -56,28 +56,28 @@ class Holder extends Component {
           // refs={(ref)=> this.upButton = ref}
           onMouseDown={() => this.setTimeVideo("up")}
           onMouseUp={() => this.setStopVideo()}
-        ></img>
+        />
         <img
           src={left}
           className="img-size-left"
           onClick={() => this.setVideo("left")}
           onMouseDown={() => this.setTimeVideo("left")}
           onMouseUp={() => this.setStopVideo()}
-        ></img>
+        />
         <img
           src={right}
           className="img-size-right"
           onClick={() => this.setVideo("right")}
           onMouseDown={() => this.setTimeVideo("right")}
           onMouseUp={() => this.setStopVideo()}
-        ></img>
+        />
         <img
           src={down}
           className="img-size-down"
           onClick={() => this.setVideo("down")}
           onMouseDown={() => this.setTimeVideo("down")}
           onMouseUp={() => this.setStopVideo()}
-        ></img>
+        />
         <div className="img-radio-zoo">
           <img
             src={zoomin}
@@ -85,14 +85,14 @@ class Holder extends Component {
             onClick={() => this.setVideo("zoomin")}
             onMouseDown={() => this.setTimeVideo("zoomin")}
             onMouseUp={() => this.setStopVideo()}
-          ></img>
+          />
           <img
             src={zoomout}
             className="img-size-zoomout"
             onClick={() => this.setVideo("zoomout")}
             onMouseDown={() => this.setTimeVideo("zoomout")}
             onMouseUp={() => this.setStopVideo()}
-          ></img>
+          />
         </div>
       </div>
     );
--- app/components/video/VideoComponent.js
+++ app/components/video/VideoComponent.js
@@ -1,4 +1,23 @@
-import React, { Component } from "react";
+import React, {Component } from "react";
+
+import {H5sPlayerWS} from './h5splayer';
+import Holder from "./Holder";
+import * as ReactDOM from "react-dom";
+
+const styles = {
+  videoStyle: {
+    width: '100%',
+    height: '100%',
+    border: '1px solid black',
+    backgroundColor: '#000000'
+  },
+  fullScreen: {
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    zIndex: 998
+  }
+};
 
 class VideoComponent extends Component {
   constructor(props) {
@@ -12,6 +31,8 @@ class VideoComponent extends Component {
           : null,
       token: props.token,
       type: props.type,
+      containerStyle: null,
+      fullScreen: false,
     };
   }
 
@@ -21,27 +42,111 @@ class VideoComponent extends Component {
     if (videoControl && this.state.session === null) {
       videoControl
         .login(null, null)
-        .then((sessionId) => this.setState({ session: sessionId }));
+        .then((sessionId) => {
+           this.setState({ session: sessionId });
+        });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (this.state.session && this.props.token !== prevProps.token) {
+      this.updateVideo(this.props.token, this.state.session);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.video) {
+      this.video.disconnect();
+    }
+  }
+
+  updateVideo(token, session) {
+
+    if (this.video) {
+      try {
+        this.video.disconnect();
+        this.video.video.src = "";
+        delete this.video;
+      } catch (e) {
+        console.log(e);
+      }
+    }
+
+    let videoConfig = {
+      videoid: 'h5sVideo1',
+      protocol: 'http:', //'http:' or 'https:'
+      host: '218.56.180.250:9108', //'172.19.112.74:8888'
+      rootpath: '/', // '/' or window.location.pathname
+      token: token,
+      streamprofile: 'main',
+      hlsver: 'v1', //v1 is for ts, v2 is for fmp4
+      session: session//session got from login
+    };
+
+    this.video = new H5sPlayerWS(videoConfig);
+
+    if (this.video) {
+      this.video.connect();
     }
   }
 
   render() {
-    return (
-      <>
-        {this.state.session !== null ? (
-          <iframe
-            src={`http://218.56.180.250:9110/video/index.html?sessionId=${this.state.session}&token=${this.props.token}&type=${this.state.type}`}
-            style={this.props.style}
-            scrolling="no"
-            frameBorder="0"
-            allowFullScreen={true}
-            controls={true}
-            webkitallowfullscreen={true.toString()}
-            mozallowfullscreen={true.toString()}
-          />
-        ) : null}
-      </>
-    );
+    let newStyle = this.state.fullScreen ? {...styles.fullScreen} : null;
+
+    if (newStyle !== null) {
+      newStyle.width = window.innerWidth;
+      newStyle.height = window.innerHeight;
+    }
+
+    let ele = <>
+      <div style={newStyle}>
+        <video
+            style={styles.videoStyle}
+            id="h5sVideo1"
+            muted
+            autoPlay
+            webkit-playsinline
+            playsinline
+            onDoubleClick={this.onMouseDblClick.bind(this)} />
+      </div>
+      {/*{this.state.session !== null ? (*/}
+      {/*  <iframe*/}
+      {/*    src={`http://218.56.180.250:9110/video/index.html?sessionId=${this.state.session}&token=${this.props.token}&type=${this.state.type}`}*/}
+      {/*    style={this.props.style}*/}
+      {/*    scrolling="no"*/}
+      {/*    frameBorder="0"*/}
+      {/*    allowFullScreen={true}*/}
+      {/*    controls={true}*/}
+      {/*    webkitallowfullscreen={true.toString()}*/}
+      {/*    mozallowfullscreen={true.toString()}*/}
+      {/*  />*/}
+      {/*  //   <iframe*/}
+      {/*  //       src={`http://218.56.180.250:9108/ws.html?session=${this.state.session}&token=${this.props.token}&type=${this.state.type}`}*/}
+      {/*  //       style={this.props.style}*/}
+      {/*  //       scrolling="no"*/}
+      {/*  //       frameBorder="0"*/}
+      {/*  //       allowFullScreen={true}*/}
+      {/*  //       controls={true}*/}
+      {/*  //       webkitallowfullscreen={true.toString()}*/}
+      {/*  //       mozallowfullscreen={true.toString()}*/}
+      {/*  //   />*/}
+      {/*) : null}*/}
+      <Holder token={this.props.token}/>
+    </>;
+
+
+    if (this.state.fullScreen) {
+      this.fullScreenComponment = ele;
+      return ReactDOM.createPortal(ele, document.body);
+    } else {
+      return ele;
+    }
+  }
+
+  onMouseDblClick() {
+    this.setState({fullScreen: !this.state.fullScreen}, () => {
+      this.updateVideo(this.props.token, this.state.session);
+    });
   }
 }
 
