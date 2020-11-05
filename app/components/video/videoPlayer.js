3363e77b04635e8ee45bda047325245c091243d3
phz <phz@tuhuitech.cn>
2020-11-05 10:25:05 +0800

    修改视频监控云台控制功能

--- app/components/video/videoPlayer.js
+++ app/components/video/videoPlayer.js
@@ -1,30 +1,18 @@
 import React from "react";
 import VideoComponent from "./VideoComponent";
 import VideoControl from "./VideoControl";
-import Holder from "./Holder";
 import "./style.scss";
 const VideoPlayer = (props) => {
   const videoControl = new VideoControl();
-  const { strtoken, style, holderStyle } = props;
+  const { strtoken, style} = props;
   return (
     <div className="video-player">
       <VideoComponent
-        style={
-          style || {
-            width: "620px",
-            height: "357px",
-            transform: "scale(0.9)",
-            position: "absolute",
-            left: "-30px",
-          }
-        }
+          style={style}
         videoControl={videoControl}
         token={strtoken}
         type={0}
       />
-      {/* <div className="video-control"> */}
-      {/*<Holder token={strtoken}></Holder>*/}
-      {/* </div> */}
     </div>
   );
 };
